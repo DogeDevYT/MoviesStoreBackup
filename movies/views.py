90e7f002446ca63cbc5b8aab84f8d18da28b45a2
DogeDevYT <52612977+DogeDevYT@users.noreply.github.com>
2025-09-17 22:19:20 -0400

    import jsonresponse

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Movie, Review
 from django.db.models import Count
